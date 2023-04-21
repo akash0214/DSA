d0b8f9975a0530bc4dc8103433895df115e7f8c6
Akash Singh <furiousakash111@gmail.com>
2023-04-21 22:27:35 +0530

    Wave printing a 2d array

---
+++ 2-D Arrays/wavePrint.cpp
@@ -0,0 +1,27 @@
+//Wave print : Print a column top to down then next down to top and so on...
+#include <iostream>
+void wavePrint(int arr[][3], int row, int col){
+    std::cout<<"The wave style print of the array is : ";
+    for(int i = 0; i < col; i++){
+        int j = 0;
+        if(i % 2 != 0){
+            j = row-1;
+            while(j >= 0){
+                std::cout<<arr[j][i]<<" ";
+                j--;
+            }
+        }
+        else{
+            while(j < row){
+                std::cout<<arr[j][i]<<" ";
+                j++;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+    wavePrint(arr, 3, 3);
+    return 0;
+}
